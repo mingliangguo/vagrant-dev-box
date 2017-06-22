60d94c7a44c2b02b13e2c5797ae21bc96347d26e
mingliangguo <mingliangguo@gmail.com>
2017-06-22 09:56:48 -0400

    separate cassandra setup to a dedicated script

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,6 +33,7 @@ echo "\n ---------- install python tools---------- \n"
 sudo apt-get install python-pip
 pip install --upgrade pip
 pip install git+https://github.com/jeffkaufman/icdiff.git
+pip install jsbeautifier
 echo "\n----- DONE ------\n"
 
 # echo "\n----- Installing Java 8 ------\n"
@@ -67,24 +68,5 @@ echo "\n----- DONE ------\n"
 echo "\n----- Installing maven and gradle ------\n"
 sudo apt-get install -y maven
 sudo apt-get install -y gradle
-echo "\n----- DONE ------\n"
-
-echo "\n----- Installing Cassandra ------\n"
-# refer to http://cassandra.apache.org/download/
-echo "deb http://www.apache.org/dist/cassandra/debian 310x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
-sudo apt-get update
-# sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
-sudo apt-get install cassandra
-
-# open ports for cassandra to run in the private ip address, i.e. not localhost
-sudo ufw allow 8888
-sudo ufw allow 7000
-sudo ufw allow 7001
-sudo ufw allow 7199
-sudo ufw allow 9042
-sudo ufw allow 9160
-sudo ufw allow 61620
-sudo ufw allow 61621
 echo "\n----- DONE ------\n"
 
